d3bec95c2ce4a317f1668ede0a2ba23954bc0f3f
ArsalJafri <154714497+ArsalJafri@users.noreply.github.com>
2024-09-05 18:05:21 -0500

    new Resume

--- src/constant.js
+++ src/constant.js
@@ -9,7 +9,7 @@ export const dialogueData = {
     "cs-degree": `This is my CS degree....it's a work in progress.`,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like : <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> and <a href="https://www.youtube.com/@coderized" target="_blank">Coderized</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often forget them.`,
-    resume: `This is my desk and on it is my <a href="/ArsalResume.pdf" target="_blank">resume.</a> Check it out?`,
+    resume: `This is my desk and on it is my <a href="/Rover_Resume.pdf" target="_blank">resume.</a> Check it out?`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
     library: `There are a lot of books on my shelves. Usually they're about history, but I recommend my favorite book Modern Prometheus by Mary Shelley.`,
